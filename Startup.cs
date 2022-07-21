6e5694df4c649d0b63006118d9b3e3b646a61edc
AveryVi <109644613+AveryVi@users.noreply.github.com>
2022-07-21 10:04:14 +0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using AmlexTradeWeb.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmlexTradeWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+
+        .AddCookie(options =>
+        {
+            options.LoginPath = "/login";
+            options.LogoutPath = "/signout";
+        })
+
+        .AddOpenId("StackExchange", "StackExchange", options =>
+        {
+            options.Authority = new Uri("https://openid.stackexchange.com/");
+            options.CallbackPath = "/signin-stackexchange";
+        }).AddSteam();
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ForumDB>(options =>
+                options.UseSqlServer(connection));
+            services.AddControllersWithViews();
+
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseRouting();
+           
+            app.UseAuthentication();
+            app.UseAuthorization();
+            //app.UseMiddleware<AuthenticationMiddleware>();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}");
+                endpoints.MapControllerRoute(name: "wikiplugins", pattern: "{controller=Wiki}/{action=Plugin}/{Name}");
+                endpoints.MapControllerRoute(name: "defaultwiki", pattern: "{controller=Wiki}/{action=Index}");
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
